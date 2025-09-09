f7ed5612cab04821b1b55d28904fb3e2ab6fa2d7
mjoshipoc-cpu <mjoshipoc@gmail.com>
2025-09-09 11:58:35 +0000

    task2

---
+++ task2.py
@@ -0,0 +1,5 @@
+
+################################Task 2#############################
+firstname=input("please enter first name:")
+lastname=input("please enter last name:")
+print(f"Hello {firstname} {lastname} Welcome to python program")
